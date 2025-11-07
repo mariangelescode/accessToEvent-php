0618451b907d788fce4e1a1d1e0d93b2041434dd
Mari Percastre <mariangelescode>
2025-11-06 19:12:33 -0600

    conection

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return (object) [
-    'db_host' => 'db',
-    'db_user' => 'mampuser',
-    'db_pass' => 'mamp123',
-    'db_name' => 'access',
+    'db_host' => 'srv1228.hstgr.io',
+    'db_user' => 'u460553087_4cc3s',
+    'db_pass' => '4cc3sToevent',
+    'db_name' => 'u460553087_acces',
     'storage_qr' => __DIR__ . '/storage/qr',
     'storage_pdf' => __DIR__ . '/storage/pdf'
 ];
