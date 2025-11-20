e65ce000417b572745032b5d074cf91807918fe9
Mari Percastre <mariangelescode>
2025-11-20 17:28:17 -0600

    qr lector

--- app/views/validate.php
+++ app/views/validate.php
@@ -4,13 +4,15 @@
 <meta charset="UTF-8">
 <title>Validar QR</title>
 <link rel="stylesheet" href="../../public/css/styles.css">
-<script src="https://unpkg.com/html5-qrcode"></script>
 </head>
 <body>
   
   <p class="title__validate">Escanea el código QR</p>
   <div id="reader"></div>
-
+  
+  <script src="https://unpkg.com/html5-qrcode"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="../../public/script/script.js"></script>
+
 </body>
 </html>
