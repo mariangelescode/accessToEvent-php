848246b21fe3213c959d7ca050ea223a8515efa3
Mari Percastre <mariangelescode>
2025-11-21 20:27:15 -0600

    count

--- app/views/validate.php
+++ app/views/validate.php
@@ -18,7 +18,9 @@
   
   <script src="https://unpkg.com/html5-qrcode"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="../../public/script/script.js"></script>
+  <!-- <script src="../../public/script/script.js"></script> -->
+   <script src="/access/public/script/script.js"></script>
+
 
 </body>
 </html>
