67ea95ecd580f7f8325d884ed60f1da4c74b147e
Mari Percastre <mariangelescode>
2025-11-22 01:27:11 -0600

    count

--- public/script/script.js
+++ public/script/script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function processQR(qr) {
 
-    fetch("/access/public/index.php?p=validate_check", {
+    fetch("./index.php?p=validate_check", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: "qr=" + encodeURIComponent(qr)
